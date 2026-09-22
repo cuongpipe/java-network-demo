ddc48a4a32675e6748428addab3cda110224d01b
longcuongctb <longcuongctb@gmail.com>
2026-09-22 13:18:23 +0000

    up sreenshot

--- src/main/java/com/gpcoder/net/URLConnectionExample.java
+++ src/main/java/com/gpcoder/net/URLConnectionExample.java
@@ -1,11 +1,9 @@
 package com.gpcoder.net;
- 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
- 
 public class URLConnectionExample {
     public static void main(String[] args) {
         try {
